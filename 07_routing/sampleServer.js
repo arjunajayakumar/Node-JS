599f128019b3fdf07457b57e98888fa04f2e3209
arjun <arjune64@gmail.com>
2020-11-17 09:43:26 +0530

    routing completed

--- 07_routing/sampleServer.js
+++ 07_routing/sampleServer.js
@@ -1,23 +1,27 @@
 const http = require("http")
 const fs = require("fs")
+const url = require("url")
 
 http.createServer((req, res) => {
-    if (req.url === "/") {
+    const q = url.parse(req.url, true)
+    console.log(q.path)
+    if (q.pathname === "/") {
         fs.readFile("sample.html", (err, data) => {
             res.writeHead(200, { "content": "text/html" })
             res.write(data)
             res.end()
 
         })
-    } else if (req.url === "/signup") {
+    } else if (q.pathname === "/signup") {
         fs.readFile("signup.html", (err, data) => {
             res.write(data)
             res.end()
 
         })
 
-    } else if (req.url === "/signupaction") {
-        res.write("Action!!!!")
+    } else if (q.pathname === "/signupaction") {
+        res.writeHead(200, { "content-type": "text/html" })
+        res.write("<h1>" + q.query.fName + "</h1>")
         res.end()
 
     } else {
