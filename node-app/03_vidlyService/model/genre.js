892ae5477c8f96b56eef9db9fb936806d29e246f
arjun <arjune64@gmail.com>
2020-11-25 20:42:03 +0530

    refactored genres

---
+++ node-app/03_vidlyService/model/genre.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Joi = require('joi')
+
+const Genre = mongoose.model('Genre', new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    }
+}))
+
+const validateGenre = (genre) => {
+    const schema = { name: Joi.string().min(4).required() }
+    return Joi.validate(genre, schema)
+}
+
+exports.Genre = Genre;
+exports.validate = validateGenre;
--- node-app/03_vidlyService/routes/genres.js
+++ node-app/03_vidlyService/routes/genres.js
@@ -1,18 +1,6 @@
-const mongoose = require('mongoose')
 const express = require('express')
-const Joi = require('joi')
+const { Genre, validate } = require('../model/genre')
 const router = express.Router()
-
-
-const Genre = mongoose.model('Genre', new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 50
-    }
-}))
-
 
 router.get('/', async (req, res) => {
     const genres = await Genre.find().sort('name')
@@ -20,7 +8,7 @@ router.get('/', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-    const { error } = validateGenre(req.body)
+    const { error } = validate(req.body)
     if (error) return res.send(error.details[0].message)
 
     let genre = new Genre({ name: req.body.name })
@@ -29,7 +17,7 @@ router.post('/', async (req, res) => {
 })
 
 router.put('/:id', async (req, res) => {
-    const { error } = validateGenre(req.body)
+    const { error } = validate(req.body)
     if (error) return res.send(error.details[0].message)
 
     const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true })
@@ -54,11 +42,5 @@ router.get('/:id', async (req, res) => {
     if (!genre) return res.status(404).send('The genre for the requested id was not found')
     res.send(genre)
 })
-
-
-const validateGenre = (genre) => {
-    const schema = { name: Joi.string().min(4).required() }
-    return Joi.validate(genre, schema)
-}
 
 module.exports = router
