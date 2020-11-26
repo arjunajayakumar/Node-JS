dc6c4025ce6289b2c53fb1664ddaa372e1fb7084
arjun <arjune64@gmail.com>
2020-11-26 13:36:58 +0530

    embedding documents

--- node-app/08_modellingRelationships/embedding.js
+++ node-app/08_modellingRelationships/embedding.js
@@ -14,46 +14,55 @@ const Author = mongoose.model('Author', authorSchema);
 
 const Course = mongoose.model('Course', new mongoose.Schema({
   name: String,
-  authors: [authorSchema]
+  author: {
+    type: authorSchema,
+    required: true
+  }
 }));
 
-async function createCourse(name, authors) {
+async function createCourse(name, author) {
   const course = new Course({
-    name, 
-    authors
-  }); 
-  
+    name,
+    author
+  });
+
   const result = await course.save();
   console.log(result);
 }
 
-async function listCourses() { 
+async function listCourses() {
   const courses = await Course.find();
   console.log(courses);
 }
 
-async function updateCourse(courseId){
+async function updateAuthor(courseId) {
   const course = await Course.update({ _id: courseId }, {
     $unset: {
       'author': ''
     }
-  });
+  })
 }
 
-async function addAuthor(courseId, author){
-  const course = await Course.findById(courseId);
-  course.authors.push(author);
-  console.log(await course.save());
-}
+updateAuthor('5fbf5d4685eb8a2a3f20f0a0')
 
-async function removeAuthor(courseId, authorId){
-  const course = await Course.findById(courseId);
-  var theAuthor = course.authors.id(authorId);
-  theAuthor.remove();
-  console.log(await course.save());
-}
 
-removeAuthor('5b8d9c752af5cb7130206e55', '5b8d9d3412a2833fc45382d2');
+
+
+
+// async function addAuthor(courseId, author) {
+//   const course = await Course.findById(courseId);
+//   course.authors.push(author);
+//   console.log(await course.save());
+// }
+
+// async function removeAuthor(courseId, authorId) {
+//   const course = await Course.findById(courseId);
+//   var theAuthor = course.authors.id(authorId);
+//   theAuthor.remove();
+//   console.log(await course.save());
+// }
+
+// removeAuthor('5b8d9c752af5cb7130206e55', '5b8d9d3412a2833fc45382d2');
 // addAuthor('5b8d9c752af5cb7130206e55', new Author({ name: 'Amy' }))
 /* createCourse('Node Course', [
   new Author({ name: 'Mosh' }),
